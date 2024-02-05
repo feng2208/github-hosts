c6b362f804c901e8aabcd4c88ffbef9fd2bb45f7
glue208 <158119438+glue208@users.noreply.github.com>
2024-02-05 17:27:59 +0800

    Add files via upload

--- github-hosts.py
+++ github-hosts.py
@@ -1,6 +1,9 @@
 # ./mitmdump -s ./github-hosts.py -p 8080
 # ./mitmdump -s ./github-hosts.py -p 8080 --set dianxin=true
 
+# sni, sni_dx, ip, ip_dx, port, port_dx: 'xxx_dx' overwrite 'xxx' when '--set dianxian=true'
+# ssl_verify, ssl_verify_dx: default to "yes"
+
 github_hosts = {
   "mappings": [
     {
@@ -27,9 +30,12 @@ from dataclasses import dataclass
 
 from mitmproxy.addonmanager import Loader
 from mitmproxy.http import HTTPFlow
-from mitmproxy.tls import ClientHelloData
+from mitmproxy import tls
 from mitmproxy.proxy.server_hooks import ServerConnectionHookData
 from mitmproxy import ctx
+
+from OpenSSL import SSL
+from mitmproxy.addons.tlsconfig import TlsConfig
 
 @dataclass
 class Mapping:
@@ -38,49 +44,21 @@ class Mapping:
     port: int
 
 
-class GithubHosts:
+class GithubHosts(TlsConfig):
     # configurations for regular ("example.com") mappings:
     host_mappings: dict[str, Mapping]
 
     # Configurations for star ("*.example.com") mappings:
     star_mappings: dict[str, Mapping]
 
+    ssl_no_verify_hosts: list[str]
+    github_hosts_loaded: bool
+
     def __init__(self) -> None:
         self.host_mappings = {}
         self.star_mappings = {}
-
-    def _load_github_hosts(self) -> None:
-        host_mappings: dict[str, Mapping] = {}
-        star_mappings: dict[str, Mapping] = {}
-
-        for mapping in github_hosts["mappings"]:
-            _sni=mapping.get("sni")
-            _ip=mapping.get("ip")
-            _port=mapping.get("port")
-            if ctx.options.dianxin:
-                if mapping.get("sni_dx") is not None:
-                    _sni=mapping.get("sni_dx")
-                if mapping.get("ip_dx") is not None:
-                    _ip=mapping.get("ip_dx")
-                if mapping.get("port_dx") is not None:
-                    _port=mapping.get("port_dx")
-
-            item = Mapping(
-                        sni=_sni, 
-                        ip=_ip,
-                        port=_port
-                   )
-            host_mappings[_sni] = item
-            if _ip is not None:
-                host_mappings[_ip] = item
-
-            for pattern in mapping["patterns"]:
-                if pattern.startswith("*."):
-                    star_mappings[pattern[2:]] = item
-                else:
-                    host_mappings[pattern] = item
-        self.host_mappings = host_mappings
-        self.star_mappings = star_mappings
+        self.ssl_no_verify_hosts = []
+        self.github_hosts_loaded = False
 
     def load(self, loader: Loader) -> None:
         loader.add_option(
@@ -96,9 +74,90 @@ class GithubHosts:
             help="set dianxin network",
         )
 
-    def configure(self, updated: set[str]) -> None:
-        if "dianxin" in updated:
+    def running(self):
+        if not self.github_hosts_loaded:
             self._load_github_hosts()
+            self.github_hosts_loaded = True
+
+    def tls_clienthello(self, data: tls.ClientHelloData) -> None:
+        data.ignore_connection = True
+        if self._get_sni(data.context.server.address[0]) is not None:
+            data.ignore_connection = False
+
+    def server_connect(self, data: ServerConnectionHookData) -> None:
+        mapping = self._get_sni(data.server.address[0])
+        if mapping is not None:
+            logging.info(f"xxxxxxx--server-host: {data.server.address[0]}")
+            if data.server.tls:
+                if mapping.ip is not None:
+                    data.server.sni = mapping.sni
+                logging.info(f"xxxxxxx--server-sni: {data.server.sni}")
+            logging.info(f"xxxxxxx--server-address: {data.server.address}")
+
+    def tls_start_server(self, tls_start: tls.TlsData) -> None:
+        super().tls_start_server(tls_start)
+        if tls_start.conn.sni in self.ssl_no_verify_hosts:
+            logging.info(f"ttttttt--tls-no-verify: {tls_start.conn.sni}")
+            tls_start.ssl_conn.set_verify(SSL.VERIFY_NONE)
+
+    def requestheaders(self, flow: HTTPFlow) -> None:
+        # We use the host header to dispatch the request:
+        target = flow.request.host_header
+        if target is None:
+            return
+
+        mapping = self._get_sni(target)
+        if mapping is not None:
+            logging.info(f"ooooooo--host: {target}")
+            flow.request.host = mapping.sni
+            if mapping.ip is not None:
+                flow.request.host = mapping.ip
+            if mapping.port is not None:
+                flow.request.port = mapping.port
+            flow.request.host_header = target
+
+    def responseheaders(self, flow: HTTPFlow) -> None:
+        flow.response.stream = True
+
+    def _load_github_hosts(self) -> None:
+        host_mappings: dict[str, Mapping] = {}
+        star_mappings: dict[str, Mapping] = {}
+        ssl_no_verify_hosts: list[str] = []
+
+        for mapping in github_hosts["mappings"]:
+            _sni = mapping.get("sni")
+            _ip = mapping.get("ip")
+            _port = mapping.get("port")
+            _ssl_verify = mapping.get("ssl_verify", "yes")
+            if ctx.options.dianxin:
+                if mapping.get("sni_dx") is not None:
+                    _sni = mapping.get("sni_dx")
+                if mapping.get("ip_dx") is not None:
+                    _ip = mapping.get("ip_dx")
+                if mapping.get("port_dx") is not None:
+                    _port = mapping.get("port_dx")
+                if mapping.get("ssl_verify_dx") is not None:
+                    _ssl_verify = mapping.get("ssl_verify_dx", "yes")
+            if _ssl_verify == "no":
+                ssl_no_verify_hosts.append(_sni)
+
+            item = Mapping(
+                        sni=_sni,
+                        ip=_ip,
+                        port=_port
+                   )
+            host_mappings[_sni] = item
+            if _ip is not None:
+                host_mappings[_ip] = item
+            for pattern in mapping["patterns"]:
+                if pattern.startswith("*."):
+                    star_mappings[pattern[2:]] = item
+                else:
+                    host_mappings[pattern] = item
+
+        self.host_mappings = host_mappings
+        self.star_mappings = star_mappings
+        self.ssl_no_verify_hosts = ssl_no_verify_hosts
 
     def _get_sni(self, host: str) -> Mapping | None:
         mapping = self.host_mappings.get(host)
@@ -117,41 +176,6 @@ class GithubHosts:
             index += 1
 
         return None
-
-    def responseheaders(self, flow: HTTPFlow) -> None:
-        flow.response.stream = True
-
-    def tls_clienthello(self, data: ClientHelloData) -> None:
-        data.ignore_connection = True
-        if self._get_sni(data.context.server.address[0]) is not None:
-            data.ignore_connection = False
-
-    def server_connect(self, data: ServerConnectionHookData) -> None:
-        mapping = self._get_sni(data.server.address[0])
-        if mapping is not None:
-            logging.info(f"xxxxxxx--server-host: {data.server.address[0]}")
-            if data.server.tls:
-                if mapping.ip is not None:
-                    data.server.sni = mapping.sni
-                logging.info(f"xxxxxxx--server-sni: {data.server.sni}")
-            logging.info(f"xxxxxxx--server-address: {data.server.address}")
-
-    def requestheaders(self, flow: HTTPFlow) -> None:
-        # We use the host header to dispatch the request:
-        target = flow.request.host_header
-        if target is None:
-            return
-
-        mapping = self._get_sni(target)
-        if mapping is not None:
-            logging.info(f"ooooooo--host: {target}")
-            if flow.request.scheme == "https":
-                flow.request.host = mapping.sni
-            if mapping.ip is not None:
-                flow.request.host = mapping.ip
-            if mapping.port is not None:
-                flow.request.port = mapping.port
-            flow.request.host_header = target
 
 
 addons = [GithubHosts()]

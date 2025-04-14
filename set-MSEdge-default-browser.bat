5b3ff3a191c723308e78b6d3bc8192ed20fbd0c1
feng2208 <158119438+feng2208@users.noreply.github.com>
2025-04-14 11:32:40 +0800

    Delete set-MSEdge-default-browser.bat

--- set-MSEdge-default-browser.bat
+++
@@ -1,5 +0,0 @@
-@ECHO off
-
-PowerShell ^
-    Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' -Name ProgId -Value 'MSEdgeHTM'; ^
-    Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -Name ProgId -Value 'MSEdgeHTM'

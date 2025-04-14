0244464e6bf18c27ae2deb21346206d4848c615e
feng2208 <158119438+feng2208@users.noreply.github.com>
2025-04-14 11:33:02 +0800

    Delete unset-proxy.bat

--- unset-proxy.bat
+++
@@ -1,5 +0,0 @@
-@ECHO off
-
-PowerShell ^
-    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyEnable -Value 0; ^
-    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyServer -Value ''
